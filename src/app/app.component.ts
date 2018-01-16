2871d21aae8313254ef90ad2ca046217d5ffca38
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-01-16 17:19:21 +0100

    radio buttons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f") signupForm : NgForm;
+  defaultQuestion = "pet";
+  answer = "";
+  genders = ["male", "female"];
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
